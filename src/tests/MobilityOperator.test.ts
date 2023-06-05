f534b65eb7b1a83bf0d2e51d0fe33a92646c2404
Kristian Selnæs <kristian@selnaes.net>
2023-06-05 12:20:42 +0200

    Add messages to texts

--- src/tests/MobilityOperator.test.ts
+++ src/tests/MobilityOperator.test.ts
@@ -2,12 +2,12 @@ import {assertType, expect, test} from 'vitest';
 import {MobilityOperator, MobilityOperatorType} from '../mobility-operators';
 
 test('MobilityOperator', () => {
-  expect(() => MobilityOperator.parse('foo')).toThrowError();
-  expect(() => MobilityOperator.parse({id: 'no name'})).toThrowError()
-  expect(() => MobilityOperator.parse({name: 'no id'})).toThrowError()
-  expect(() => MobilityOperator.parse({id: 'id', name: 'no formFactor'})).toThrowError()
-  expect(() => MobilityOperator.parse({id: 'id', name: 'no empty formFactor', formFactors: []})).toThrowError()
-  expect(MobilityOperator.parse({id: 'id', name: 'default showInApp',formFactors: ['SCOOTER']}).showInApp).toEqual(true);
+  expect(() => MobilityOperator.parse('foo'), 'Config must be an object').toThrowError();
+  expect(() => MobilityOperator.parse({id: 'foo'}), 'Name is required').toThrowError()
+  expect(() => MobilityOperator.parse({name: 'foo'}), 'Id is required').toThrowError()
+  expect(() => MobilityOperator.parse({id: 'id', name: 'foo'}), 'FormFactor is required').toThrowError()
+  expect(() => MobilityOperator.parse({id: 'id', name: 'foo', formFactors: []}), 'FormFactors must have at least one value').toThrowError()
+  expect(MobilityOperator.parse({id: 'id', name: 'default showInApp',formFactors: ['SCOOTER']}).showInApp, 'showInApp should default to true').toEqual(true);
 
   assertType<MobilityOperatorType>(
     MobilityOperator.parse({
