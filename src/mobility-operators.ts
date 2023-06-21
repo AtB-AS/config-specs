be40c938558c10f34ec0179a23ea1f13cfff80d3
Kristian Selnæs <kristian@selnaes.net>
2023-06-21 08:36:48 +0200

    feat: Mobility operators ticket bundled benefits

--- src/mobility-operators.ts
+++ src/mobility-operators.ts
@@ -1,4 +1,5 @@
 import {z} from "zod";
+import {LanguageAndTextTypeArray} from './common';
 
 export const FormFactor = z.union([
   z.literal("SCOOTER"),
@@ -6,11 +7,24 @@ export const FormFactor = z.union([
   z.literal("CAR"),
 ])
 
+
+export const OperatorBenefitId = z.literal("free-unlock");
+
+export const OperatorBenefit = z.object({
+  id: OperatorBenefitId,
+  descriptionWhenActive: LanguageAndTextTypeArray.optional(),
+  descriptionWhenNotActive: LanguageAndTextTypeArray.optional(),
+  callToAction: LanguageAndTextTypeArray.optional(),
+})
+
+export type OperatorBenefitType = z.infer<typeof OperatorBenefit>;
+
 export const MobilityOperator = z.object({
   id: z.string().nonempty(),
   name: z.string().nonempty(),
   showInApp: z.boolean().default(false),
   formFactors: z.array(FormFactor).nonempty(),
+  benefits: z.array(OperatorBenefit).optional().default([])
 });
 
 export type MobilityOperatorType = z.infer<typeof MobilityOperator>;
