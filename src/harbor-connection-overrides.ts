1006e4bff5560d5a8b0580e4610e6dcc2c259c7b
Jorge Osorio <jorelosorio@gmail.com>
2023-09-04 08:03:30 +0200

    add is free attribute

--- src/harbor-connection-overrides.ts
+++ src/harbor-connection-overrides.ts
@@ -3,6 +3,7 @@ import {z} from 'zod';
 export const HarborConnectionOverride = z.object({
   from: z.string(),
   to: z.string(),
+  isFree: z.boolean().optional(),
 });
 export type HarborConnectionOverrideType = z.infer<typeof HarborConnectionOverride>;
 
