dc6b6921a56a745bd32783559f08e2c0a342b7c3
Kristian Selnæs <kristian@selnaes.net>
2023-11-30 13:32:01 +0100

    chore: Add Type postfix to Typescript types

--- src/tests/TravelSearchTransport.test.ts
+++ src/tests/TravelSearchTransport.test.ts
@@ -1,11 +1,11 @@
 import {assertType, expect, test} from 'vitest';
-import {TransportModeFilterOptionType} from '../travel-search-filters';
+import {TransportModeFilterOption, TransportModeFilterOptionType} from '../travel-search-filters';
 
 test('TransportModeFilterOptionType', function () {
-  expect(() => TransportModeFilterOptionType.parse('foo')).toThrowError();
+  expect(() => TransportModeFilterOption.parse('foo')).toThrowError();
 
   assertType<TransportModeFilterOptionType>(
-    TransportModeFilterOptionType.parse({
+    TransportModeFilterOption.parse({
       id: 'id',
       icon: {transportMode: 'bus'},
       text: [
