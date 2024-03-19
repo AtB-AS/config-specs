51f602bc336ae702ff0c1dc01450f6356576cfd3
Ridwan Aditama <reidzeibel@gmail.com>
2024-03-19 10:19:29 +0100

    feat: add on-behalf-of field to fareProductTypeConfig (#40)
    * feat: add on-behalf-of field to fareProductTypeConfig
    
    * feat: set field onBehalfOfEnabled as required

--- src/fare-product-type.ts
+++ src/fare-product-type.ts
@@ -67,6 +67,7 @@ export const FareProductTypeConfig = z.object({
   description: LanguageAndTextTypeArray,
   configuration: FareProductTypeConfigSettings,
   isCollectionOfAccesses: z.boolean(),
+  onBehalfOfEnabled: z.boolean(),
 });
 
 export type ProductTypeTransportModes = z.infer<
