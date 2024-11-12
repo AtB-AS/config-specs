643d30f5f79fe4a673937a10a9f256a983711d30
Gøran Dalum <goran.dalum@gmail.com>
2024-11-12 15:13:03 +0100

    feat: Add StopSignalButtonConfig
    This will make it possible to override the start and end times of
    the activation window, and the modes/submodes the button is
    applicable for.
    
    All fields have default values, so it works even if nothing is
    added to Firestore.

---
+++ src/stop-signal-button-config.ts
@@ -0,0 +1,16 @@
+import {z} from 'zod';
+import {TransportModeType, TransportSubmodeType} from './common';
+
+export const StopSignalModeAndSubmodes = z.object({
+  mode: TransportModeType,
+  submodes: z.array(TransportSubmodeType).optional(),
+});
+
+export const StopSignalButtonConfig = z.object({
+  activationWindowStartMinutesBefore: z.number().default(60),
+  activationWindowEndMinutesBefore: z.number().default(2),
+  modes: z.array(StopSignalModeAndSubmodes).default([{mode: 'bus'}, {mode: 'tram'}]),
+});
+
+export type StopSignalButtonConfigType = z.infer<typeof StopSignalButtonConfig>;
+export type StopSignalModeAndSubmodesType = z.infer<typeof StopSignalModeAndSubmodes>;
