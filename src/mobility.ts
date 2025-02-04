346482a9ec25f68085aae3a2715f00fe1afada72
jorunnl <jorunn.leithe@kantega.no>
2025-02-04 15:25:44 +0100

    feat: add entire brandAssets from GBFS

--- src/mobility.ts
+++ src/mobility.ts
@@ -40,10 +40,38 @@ export const MobilityOperator = z.object({
   benefits: z.array(OperatorBenefit).optional().default([]),
   brandAssets: z
     .object({
-      brandImageUrl: z.string().describe('base64 logo'),
+      brandLastModified: z
+        .date()
+        .describe(
+          'Date that indicates the last time any included brand assets were updated or modified.',
+        ),
+      brandTermsUrl: z
+        .string()
+        .url()
+        .optional()
+        .describe(
+          'A fully qualified URL pointing to the location of a page that defines the license terms of brand icons, colors, or other trademark information. This field MUST NOT take the place of license_url or license_id.',
+        ),
+      brandImageUrl: z
+        .string()
+        .describe(
+          'A fully qualified URL pointing to the location of a graphic file representing the brand for the service. File MUST be in SVG V1.1 format and MUST be either square or round.',
+        ),
+      brandImageUrlDark: z
+        .string()
+        .optional()
+        .describe(
+          'A fully qualified URL pointing to the location of a graphic file representing the brand for the service for use in dark mode applications. File MUST be in SVG V1.1 format and MUST be either square or round.',
+        ),
+      color: z
+        .string()
+        .optional()
+        .describe(
+          'Color used to represent the brand for the service expressed as a 6 digit hexadecimal color code in the form #000000.',
+        ),
     })
     .optional()
-    .describe('modeled like brandAssets in GBFS'),
+    .describe('modeled 1-1 like brandAssets in GBFS'),
 });
 
 export type MobilityOperatorType = z.infer<typeof MobilityOperator>;
