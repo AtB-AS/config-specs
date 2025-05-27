e18995cdbb21a5d840ce1949e9138c9fc5d26eac
Johannes Røsvik <j.rosvik@gmail.com>
2025-05-27 15:51:28 +0200

    feat: add nullish `isFree`

--- src/harbor-connection-overrides.ts
+++ src/harbor-connection-overrides.ts
@@ -3,7 +3,7 @@ import {z} from 'zod';
 export const HarborConnectionOverride = z.object({
   from: z.string(),
   to: z.string(),
-  isFree: z.boolean().optional(),
+  isFree: z.boolean().nullish(),
 });
 export type HarborConnectionOverrideType = z.infer<
   typeof HarborConnectionOverride
