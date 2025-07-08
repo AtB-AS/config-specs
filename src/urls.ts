63c773d6d652f06e13fa0d2c5795530e5869c0b3
Jorge Osorio <jorelosorio@gmail.com>
2025-07-08 16:02:40 +0200

    feat: Add contact URLs

--- src/urls.ts
+++ src/urls.ts
@@ -15,6 +15,10 @@ export const ConfigurableLinks = z.object({
   tileServerBaseUrl: LanguageAndTextTypeArray.optional(),
   mapboxSpriteUrl: LanguageAndTextTypeArray.optional(),
   mobilityTermsUrl: LanguageAndTextTypeArray.optional(),
+  contactFormUrl: LanguageAndTextTypeArray.optional(),
+  lostAndFoundUrl: LanguageAndTextTypeArray.optional(),
+  refoundUrl: LanguageAndTextTypeArray.optional(),
+  frecuentlyAskedQuestionsUrl: LanguageAndTextTypeArray.optional(),
 });
 
 export type ConfigurableLinksType = z.infer<typeof ConfigurableLinks>;
