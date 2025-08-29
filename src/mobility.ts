415ea127e05ae6c0a681c383fbce59239ac36ff2
Jorunn Leithe <jorunn@selbu.org>
2025-08-29 08:46:02 +0200

    fix: remove default value for age limit

--- src/mobility.ts
+++ src/mobility.ts
@@ -54,7 +54,7 @@ export const MobilityOperator = z.object({
     .optional()
     .describe('modeled 1-1 like brandAssets in EnTur mobility API'),
   isDeepIntegrationEnabled: z.boolean().default(false),
-  ageLimit: z.number().int().positive().default(18),
+  ageLimit: z.number().int().positive().optional(),
   appUrl: z
     .object({
       ios: z.string().url(),
