35a65b4cb8870ec2faccf2e0b59f160ced9df0eb
Adrian Santana Berg <adrian_berg96@hotmail.com>
2026-04-23 10:59:53 +0200

    feat: add KnownQrCodeUrls schema and related types (#184)

---
+++ src/known-qr-code-urls.ts
@@ -0,0 +1,19 @@
+import {z} from 'zod';
+
+export const KnownQrCodeUrlOpenMode = z.enum(['in-app-browser', 'open-url']);
+
+export const KnownQrCodeUrl = z.object({
+  id: z.string(),
+  pattern: z.string().min(1),
+  openMode: KnownQrCodeUrlOpenMode,
+});
+
+export const KnownQrCodeUrls = z
+  .object({
+    urls: z.array(KnownQrCodeUrl).default([]),
+  })
+  .default({urls: []});
+
+export type KnownQrCodeUrlType = z.infer<typeof KnownQrCodeUrl>;
+export type KnownQrCodeUrlsType = z.infer<typeof KnownQrCodeUrls>;
+export type KnownQrCodeUrlOpenModeType = z.infer<typeof KnownQrCodeUrlOpenMode>;
