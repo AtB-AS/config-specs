8a67beaf779009436f1e7819f77294d8b8cab192
Adrian Santana Berg <adrian_berg96@hotmail.com>
2026-08-25 13:11:44 +0200

    fix!: remove unused BonusTextsType and BonusProductType (#201)
    * fix!: remove unused BonusTexts
    
    Co-authored-by: Jorunn Leithe <jorunn@selbu.org>
    
    * fix!: remove unused BonusProductType
    
    Co-authored-by: Jorunn Leithe <jorunn@selbu.org>
    
    ---------
    
    Co-authored-by: Jorunn Leithe <jorunn@selbu.org>

--- src/tools/specifications-types.ts
+++ src/tools/specifications-types.ts
@@ -2,8 +2,6 @@ import {z} from 'zod';
 import {FareProductGroup, FareProductTypeConfig} from '../fare-product-type';
 import {TravelSearchFilters} from '../travel-search-filters';
 import {
-  BonusProduct,
-  BonusTexts,
   ConsentLine,
   MobilityOperator,
   OperatorBenefitId,
@@ -66,8 +64,6 @@ export const schemaTypes = {
       scooterConsentLines: z.array(ConsentLine).optional(),
       bicycleConsentLines: z.array(ConsentLine).optional(),
       benefitIdsRequiringValueCode: z.array(OperatorBenefitId).optional(),
-      bonusProducts: z.array(BonusProduct).optional(),
-      bonusTexts: BonusTexts.optional(),
     })
     .meta({title: 'MobilityOperator'}),
   other: Other,
